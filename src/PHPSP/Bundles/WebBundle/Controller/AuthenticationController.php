88e530a3a37eaed2dde4f10dc01e47befa55e23c
Rafael Dohms <rdohms@gmail.com>
2011-07-30 14:56:16 -0300

    Layouting changes, chanining templates

---
+++ src/PHPSP/Bundles/WebBundle/Controller/AuthenticationController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace PHPSP\Bundles\WebBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class AuthenticationController extends Controller
+{
+    /**
+     * @Route("/login")
+     * @Template()
+     */
+    public function loginAction($name)
+    {
+        return array('name' => $name);
+    }
+
+    /**
+     * @Route("/authentication/authenticate")
+     */
+    public function authenticateAction()
+    {
+        
+    }
+    
+    /**
+     * @Route("/logout")
+     * @Template()
+     */
+    public function logoutAction()
+    {
+        
+    }
+}
